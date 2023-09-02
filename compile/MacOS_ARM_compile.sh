208e9a26a4043b46ba179e524fb34ddbe9fc7381
Sunny You <sunny.jiang.you@gmail.com>
2023-09-01 23:17:29 -0400

    Correct installation of dependencies. Update to Pygame version 2.5.1.

--- compile/MacOS_ARM_compile.sh
+++ compile/MacOS_ARM_compile.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-if [ ! -d "venv" ]; then
-    python3 -m venv . 
-    pip3 install -r requirements.txt
-fi
-source bin/activate
-
 if pwd | grep -q "compile"; then
     cd ..
 fi
 
+if [ ! -d "venv" ]; then
+    python3 -m venv . 
+    source bin/activate
+    pip3 install -r requirements.txt
+fi
+
 cd src
-python3 -m PyInstaller --onefile --noconfirm --name=cs_timer_ripoff_MacOS_ARM --clean --distpath=../ cs_timer_ripoff.py
+python3 -m PyInstaller --onefile --noconfirm --name=cs_timer_ripoff_MacOS_ARM--clean --distpath=../ cs_timer_ripoff.py
+if [ $? -ne 0 ]; then
+    python3 -m pyinstaller --onefile --noconfirm --name=cs_timer_ripoff_MacOS_ARM --clean --distpath=../ cs_timer_ripoff.py
+fi
 for f in *.spec; do
     rm $f
 done
