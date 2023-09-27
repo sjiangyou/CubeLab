1106f5cbdfd9dfa911e89872ea46abe02db6b468
Sunny You <sunny.jiang.you@gmail.com>
2023-09-27 19:35:27 -0400

    Better script to compile the code.

--- compile/MacOS_Intel_compile.sh
+++ compile/MacOS_Intel_compile.sh
@@ -4,12 +4,9 @@ if pwd | grep -q "compile"; then
     cd ..
 fi
 
-if [ ! -d "venv" ]; then
-    python3 -m venv . 
-    source bin/activate
-    pip3 install -r requirements.txt
-fi
-
+python3 -m venv . 
+source bin/activate
+pip3 install -r requirements.txt
 cd src
 python3 -m PyInstaller --onefile --noconfirm --name=cs_timer_ripoff_MacOS_Intel --clean --distpath=../ cs_timer_ripoff.py
 if [ $? -ne 0 ]; then
